a95831f8351fac1afb2adb96b671dd64ff7173e0
Finallion <74151350+finallion@users.noreply.github.com>
2021-11-21 23:50:52 +0100

    Minor tweaks to biome fog.

--- src/main/java/com/finallion/graveyard/events/BiomeEvents.java
+++ src/main/java/com/finallion/graveyard/events/BiomeEvents.java
@@ -1,8 +1,11 @@
 package com.finallion.graveyard.events;
 
 import com.finallion.graveyard.TheGraveyard;
+import com.finallion.graveyard.init.TGBiomes;
+import net.minecraft.block.SoundType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,28 +14,36 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = TheGraveyard.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class BiomeEvents {
     private static double fogDensity;
+    private static final ResourceLocation HAUNTED_FOREST_LOC = TGBiomes.HAUNTED_FOREST.getRegistryName();
+    private static final ResourceLocation HAUNTED_LAKES_LOC = TGBiomes.HAUNTED_LAKES.getRegistryName();
+    private static final ResourceLocation ERODED_FOREST_LOC = TGBiomes.ERODED_HAUNTED_FOREST.getRegistryName();
 
     @SubscribeEvent
     public static void FogDensityEvent(EntityViewRenderEvent.FogDensity event) {
         BlockPos pos = event.getRenderer().getMainCamera().getBlockPosition();
-        String biomeName = event.getInfo().getEntity().level.getBiome(pos).getRegistryName().toString();
+        ResourceLocation location = event.getInfo().getEntity().level.getBiome(pos).getRegistryName();
+
 
         if (!TheGraveyard.CONFIG.haunted_lakes_fog.get() && !TheGraveyard.CONFIG.eroded_haunted_forest_fog.get() && !TheGraveyard.CONFIG.haunted_forest_fog.get()) {
             return;
         }
 
+        if (location == null) {
+            return;
+        }
 
-        if (biomeName.contains("graveyard:haunted_lakes") && TheGraveyard.CONFIG.haunted_lakes_fog.get()) {
+
+        if (location.equals(HAUNTED_LAKES_LOC) && TheGraveyard.CONFIG.haunted_lakes_fog.get()) {
             if (pos.getY() > TheGraveyard.CONFIG.haunted_lakes_fogMaxY.get() || pos.getY() < TheGraveyard.CONFIG.haunted_lakes_fogMinY.get()) {
                 return;
             }
             fogDensity = TheGraveyard.CONFIG.haunted_lakes_fog_density.get();
-        } else if (biomeName.contains("graveyard:eroded_haunted") && TheGraveyard.CONFIG.eroded_haunted_forest_fog.get()) {
+        } else if (location.equals(ERODED_FOREST_LOC) && TheGraveyard.CONFIG.eroded_haunted_forest_fog.get()) {
             if (pos.getY() > TheGraveyard.CONFIG.eroded_haunted_forest_fogMaxY.get() || pos.getY() < TheGraveyard.CONFIG.eroded_haunted_forest_fogMinY.get()) {
                 return;
             }
             fogDensity = TheGraveyard.CONFIG.eroded_haunted_forest_fog_density.get();
-        } else if (biomeName.contains("graveyard:haunted_forest") && TheGraveyard.CONFIG.haunted_forest_fog.get()) {
+        } else if (location.equals(HAUNTED_FOREST_LOC) && TheGraveyard.CONFIG.haunted_forest_fog.get()) {
             if (pos.getY() > TheGraveyard.CONFIG.haunted_forest_fogMaxY.get() || pos.getY() < TheGraveyard.CONFIG.haunted_forest_fogMinY.get()) {
                 return;
             }
--- src/main/java/com/finallion/graveyard/init/TGBiomes.java
+++ src/main/java/com/finallion/graveyard/init/TGBiomes.java
@@ -14,11 +14,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = TheGraveyard.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TGBiomes {
+    /*
     private static final Map<ResourceLocation, Biome> BIOMES = new HashMap<>();
 
     public static final RegistryKey<Biome> HAUNTED_FOREST = add("haunted_forest", HauntedForestBiomes.HauntedForestBiome());
@@ -51,5 +54,36 @@ public class TGBiomes {
         }
 
         BiomeDictionary.addTypes(key, types);
+    }
+
+
+     */
+
+    public static List<Biome> BIOMES = new ArrayList<>();
+
+    public static Biome HAUNTED_FOREST = createBiome("haunted_forest", HauntedForestBiomes.HauntedForestBiome());
+    public static Biome HAUNTED_LAKES = createBiome("haunted_lakes", HauntedForestBiomes.HauntedForestLakeBiome());
+    public static Biome ERODED_HAUNTED_FOREST = createBiome("eroded_haunted_forest", HauntedForestBiomes.ErodedHauntedForestBiome());
+
+    public static Biome createBiome(String id, Biome biome) {
+        ResourceLocation resourceLocation = new ResourceLocation(TheGraveyard.MOD_ID, id);
+
+        biome.setRegistryName(resourceLocation);
+
+        BIOMES.add(biome);
+
+        return biome;
+    }
+
+    public static void addToDictionaries() {
+        BIOMES.forEach((biome -> BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(RegistryKey.create(Registry.BIOME_REGISTRY, biome.getRegistryName()), 2))));
+        BIOMES.forEach((biome -> BiomeDictionary.addTypes(RegistryKey.create(Registry.BIOME_REGISTRY, biome.getRegistryName()), BiomeDictionary.Type.OVERWORLD)));
+    }
+
+
+    @SubscribeEvent
+    public static void registerBiomes(RegistryEvent.Register<Biome> event) {
+        addToDictionaries();
+        BIOMES.forEach(biome -> event.getRegistry().register(biome));
     }
 }
