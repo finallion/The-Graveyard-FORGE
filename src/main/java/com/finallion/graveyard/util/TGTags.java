08e93677932221fea91ea2846247896f7365cce0
Finallion <74151350+finallion@users.noreply.github.com>
2022-01-26 16:00:24 +0100

    Updated tag list for coral blocks

--- src/main/java/com/finallion/graveyard/util/TGTags.java
+++ src/main/java/com/finallion/graveyard/util/TGTags.java
@@ -1,15 +1,16 @@
 package com.finallion.graveyard.util;
 
 import com.finallion.graveyard.TheGraveyard;
-import net.fabricmc.fabric.api.tag.TagFactory;
-import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.level.block.Block;
+
 
 public class TGTags {
 
-    public static final Tag<Block> DEAD_CORAL_BLOCKS = TagFactory.BLOCK.create(new Identifier(TheGraveyard.MOD_ID, "dead_coral_blocks"));
-    public static final Tag<Block> DEAD_CORAL_PLANTS = TagFactory.BLOCK.create(new Identifier(TheGraveyard.MOD_ID, "dead_coral_plants"));
-    public static final Tag<Block> DEAD_CORALS = TagFactory.BLOCK.create(new Identifier(TheGraveyard.MOD_ID, "dead_corals"));
-    public static final Tag<Block> DEAD_WALL_CORALS = TagFactory.BLOCK.create(new Identifier(TheGraveyard.MOD_ID, "dead_wall_corals"));
+    public static final Tag.Named<Block> DEAD_CORAL_BLOCKS = BlockTags.bind("graveyard:dead_coral_blocks");
+    public static final Tag.Named<Block> DEAD_CORAL_PLANTS = BlockTags.bind("graveyard:dead_coral_plants");
+    public static final Tag.Named<Block> DEAD_CORALS = BlockTags.bind("graveyard:dead_corals");
+    public static final Tag.Named<Block> DEAD_WALL_CORALS = BlockTags.bind("graveyard:dead_wall_corals");
 }
