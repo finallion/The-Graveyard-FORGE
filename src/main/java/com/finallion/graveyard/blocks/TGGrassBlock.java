e58045096348894d4faf78b11c2f67298af8d003
Finallion <74151350+finallion@users.noreply.github.com>
2022-08-01 14:31:30 +0200

    Made grass blocks turn into path blocks by using shovel.

--- src/main/java/com/finallion/graveyard/blocks/TGGrassBlock.java
+++ src/main/java/com/finallion/graveyard/blocks/TGGrassBlock.java
@@ -4,9 +4,14 @@ package com.finallion.graveyard.blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+import org.jetbrains.annotations.Nullable;
 
 public class TGGrassBlock extends GrassBlock {
 
@@ -21,8 +26,12 @@ public class TGGrassBlock extends GrassBlock {
         return new ItemStack(Items.GRASS_BLOCK);
     }
 
+    @Override
+    public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate) {
+        if (ToolActions.SHOVEL_FLATTEN == toolAction) {
+            return Blocks.DIRT_PATH.defaultBlockState();
+        }
 
-
-
-
+        return super.getToolModifiedState(state, context, toolAction, simulate);
+    }
 }
